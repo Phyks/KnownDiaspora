4252e89561da0b7101b10a536fdffe0a4c9b3516
Phyks <webmaster@phyks.me>
2015-01-18 00:20:34 +0100

    Forgot to add renamed files

---
+++ templates/default/account/diaspora.tpl.php
@@ -0,0 +1,48 @@
+<div class="row">
+
+    <div class="span10 offset1">
+        <?= $this->draw('account/menu') ?>
+        <h1>Diaspora</h1>
+
+    </div>
+
+</div>
+<div class="row">
+    <div class="span10 offset1">
+        <div class="row">
+            <div class="span6">
+                <p>
+                    Easily share pictures, updates, and posts to Diaspora.</p>
+
+                <p>
+                    With Diaspora connected, you can cross-post content that you publish publicly on
+                    your site.
+                </p>
+            </div>
+        </div>
+        <form class="form-horizontal" method="post" action="<?= \Idno\Core\site()->config()->getDisplayURL() ?>account/diaspora/">
+            <div class="row">
+                <div class="span1"><p><strong><label class="control-label" for="pod">Pod</label></strong></p></div>
+                <div class="span3"><input type="text" name="pod" id="pod" value="<?=htmlspecialchars(\Idno\Core\site()->config()->diaspora['diaspora_pod'])?>"/></div>
+                <div class="span3"><p class="config-desc">Your Diaspora pod.</p></div>
+            </div>
+            <div class="row">
+                <div class="span1"><p><strong><label class="control-label" for="user">Username</label></strong></p></div>
+                <div class="span3"><input type="text" name="user" id="user" value="<?=htmlspecialchars(\Idno\Core\site()->config()->diaspora['diaspora_username'])?>"/></div>
+                <div class="span3"><p class="config-desc">Your Diaspora username.</p></div>
+            </div>
+            <div class="row">
+                <div class="span1"><p><strong><label class="control-label" for="pass">Password</label></strong></p></div>
+                <div class="span3"><input type="password" name="pass" id="pass"/></div>
+                <div class="span3"><p class="config-desc">Your Diaspora password.</p></div>
+            </div>
+            <div class="control-group">
+                <div class="controls-save">
+                    <button type="submit" class="btn btn-primary">Save updates</button>
+                </div>
+            </div>
+            <?= \Idno\Core\site()->actions()->signForm('/account/diaspora/')?>
+        </form>
+        <p><a href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>account/diaspora/?remove">Remove</a> your credentials.</p>
+    </div>
+</div>
---
+++ templates/default/account/diaspora/menu.tpl.php
@@ -0,0 +1,1 @@
+<li <?php if ($_SERVER['REQUEST_URI'] == '/account/diaspora/') echo 'class="active"'; ?>><a href="/account/diaspora/">Diaspora</a></li>
